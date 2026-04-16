97beffe5d2e111a58f9612efd2c44c816fbfd679
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-04-16 07:26:07 +0430

    Quantity + Total Price

--- product-store/src/pages/Cart.jsx
+++ product-store/src/pages/Cart.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart, increaseQuantity, decreaseQuantity } from '../store/cartSlice';
 
 function Cart() {
     const { items } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
+    const totalPrice = items.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+    );
     return (
         <div>
             <h1>Cart</h1>
 
-            {items.lenght === 0 ? (
+            {items.length === 0 ? (
                 <p>Your Cart is empty</p>
             ) : (
                 items.map((item) => (
                     <div key={item.id}>
                     <p>{item.title}</p>
-                    <p>Quantity: {item.quantity}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => dispatch(decreaseQuantity(item.id))}> - </button>
+                    <span>{item.quantity}</span>
+                    <button onClick={() => dispatch(increaseQuantity(item.id))}> + </button>
+                    <button onClick={() => dispatch(removeFromCart(item.id))}> Remove </button>
                    </div>
                 ))
             )}
+
+            <h2>Total: ${totalPrice.toFixed(2)}</h2>
         </div>
     );
 }
--- product-store/src/store/cartSlice.js
+++ product-store/src/store/cartSlice.js
@@ -25,9 +25,33 @@ const cartSlice = createSlice({
         (item) => item.id !== action.payload
       );
     },
+    increaseQuantity: (state, action) => {
+  const item = state.items.find(
+    (item) => item.id === action.payload
+  );
+  if (item) {
+    item.quantity += 1;
+  }
+},
+
+decreaseQuantity: (state, action) => {
+  const item = state.items.find(
+    (item) => item.id === action.payload
+  );
+
+  if (item) {
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    } else {
+      state.items = state.items.filter(
+        (i) => i.id !== action.payload
+      );
+    }
+  }
+},
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
