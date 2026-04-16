eed251c34dce93a89a2be4b6b84c1e231b57367a
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-04-16 06:57:15 +0430

    Cart Page UI

--- product-store/src/store/cartSlice.js
+++ product-store/src/store/cartSlice.js
@@ -19,8 +19,15 @@ const cartSlice = createSlice({
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
+
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
 export default cartSlice.reducer;
